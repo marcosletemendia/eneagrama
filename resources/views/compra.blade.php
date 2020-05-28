04ca110a981c95bf02e7f381d2263631515f6228
tomasgilles <tomas.gilles@gmail.com>
2020-05-28 16:31:10 -0300

    update

--- resources/views/compra.blade.php
+++ resources/views/compra.blade.php
@@ -7,8 +7,8 @@
   <div class="central">
     <h2>Elija el Metodo de Pago</h2>
     <div class="pago">
-      <a href="/checkout/preferences"><img src="/storage/mercadopago.jpg" alt="">mp</a>
-      <a href="/paypal"><img src="/storage/paypal.jpg" alt="">paypal</a>
+      <a href="/checkout/preferences"><img src="/img/mercadopago.jpg" alt=""></a>
+      <a href="/paypal"><img src="/img/paypal.jpg" alt=""></a>
     </div>
 
   </div>
