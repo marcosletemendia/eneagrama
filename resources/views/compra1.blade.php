787082f66c72bdcc62bf479894a6750411d200ae
Marcos <marcos.letemendia@gmail.com>
2020-05-30 12:55:19 -0300

    boton de pago

--- resources/views/compra1.blade.php
+++ resources/views/compra1.blade.php
@@ -7,7 +7,13 @@
   <div class="central">
     <h2>Vista Previa</h2>
     <div class="pago">
-      
+
+      {{-- BOTON DE PAGO --}}
+      <script src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
+      data-preference-id="227294504-0e47e8e7-d234-466f-9356-29fb18341cea">
+      </script>
+
+
     </div>
   </div>
 @endsection
